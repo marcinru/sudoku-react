997e36dbc6e38087fc1a4a06f3943701c27d2fd6
Marcin Er <mar.rudzki@gmail.com>
2026-03-17 06:35:18 +0100

    implement board state management and control click handling

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,19 +1,12 @@
+import type { Cells } from './App';
 import { Cell } from './Cell';
 import { useState } from 'react';
 
-const cells = [
-  [5, 7, null, 1, 8, null, null, null, 3],
-  [null, 8, 6, null, null, null, 4, null, 2],
-  [null, 9, 2, 6, 7, null, 8, null, null],
-  [null, 1, null, 8, 6, 3, null, 4, 7],
-  [4, 2, null, null, 1, null, null, null, null],
-  [null, null, 7, null, 2, null, 1, 9, null],
-  [7, null, 3, 2, 9, 8, 5, 1, 6],
-  [null, null, null, 5, null, null, null, 8, 4],
-  [8, 5, null, 7, null, 6, null, null, 9],
-];
+interface BoardProps {
+  board: Cells;
+}
 
-export function Board() {
+export function Board({ board }: BoardProps) {
   const [selectedRow, setSelectedRow] = useState<number | null>(null);
   const [selectedCol, setSelectedCol] = useState<number | null>(null);
 
@@ -28,7 +21,7 @@ export function Board() {
       <colgroup span={3}></colgroup>
       <colgroup span={3}></colgroup>
       <tbody>
-        {cells.map((row, i) => (
+        {board.map((row, i) => (
           <tr key={`row${i}`}>
             {row.map((cell, j) => (
               <Cell
--- src/Controls.tsx
+++ src/Controls.tsx
@@ -1,11 +1,23 @@
 const numbers = Array.from({ length: 9 }, (_, i) => i + 1);
 
-export function Controls() {
+interface ControlsProps {
+  onClick: (num: number) => void;
+}
+
+export function Controls({ onClick }: ControlsProps) {
   return (
     <div id="controls">
       <div id="number-controls">
         {numbers.map((number) => (
-          <button className="number-control">{number}</button>
+          <button
+            className="number-control"
+            key={number}
+            onClick={() => {
+              onClick(number);
+            }}
+          >
+            {number}
+          </button>
         ))}
       </div>
       <input
